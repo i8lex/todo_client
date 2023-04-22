b806d57fb652c9acfe393ca1598a7a79fa25a9ab
i8lex <cybeerid@gmail.com>
2023-04-22 12:13:46 +0300

    fix timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -31,46 +31,12 @@ export const Timer = ({ deadline }) => {
     }
   }, [deadline]);
 
-  // useEffect(() => {
-  //   if (deadline !== "Not set") {
-  //     const deadlineMoment = moment(deadline); // Создаем момент-объект из даты
-  //     const intervalId = setInterval(() => {
-  //       const nowMoment = moment(); // Получаем текущий момент времени
-  //       const remainingTimeInMs = deadlineMoment.diff(nowMoment); // Рассчитываем оставшееся время в миллисекундах
-  //       if (remainingTimeInMs < 0) {
-  //         clearInterval(intervalId);
-  //         setRemainingTime(0);
-  //       } else {
-  //         setRemainingTime(remainingTimeInMs);
-  //       }
-  //     }, 1000);
-  //     return () => clearInterval(intervalId);
-  //   }
-  // }, [deadline]);
-  // useEffect(() => {
-  //   if (deadline) {
-  //
-  //     const deadlineDate = new Date(deadline);
-  //     const intervalId = setInterval(() => {
-  //       const now = new Date();
-  //       const remainingTimeInMs = deadlineDate - now;
-  //       if (remainingTimeInMs < 0) {
-  //         clearInterval(intervalId);
-  //         setRemainingTime(0);
-  //       } else {
-  //         setRemainingTime(remainingTimeInMs);
-  //       }
-  //     }, 1000);
-  //     return () => clearInterval(intervalId);
-  //   }
-  // }, [deadline]);
-
   useEffect(() => {
     if (deadline !== "Not set") {
-      const deadlineDate = parseISO(deadline); // Преобразуем строку даты в объект даты
+      const deadlineDate = parseISO(deadline);
       const intervalId = setInterval(() => {
-        const now = new Date(); // Создаем объект текущей даты
-        const remainingTimeInMs = deadlineDate - now; // Рассчитываем оставшееся время в миллисекундах
+        const now = new Date();
+        const remainingTimeInMs = deadlineDate - now;
         if (remainingTimeInMs < 0) {
           clearInterval(intervalId);
           setRemainingTime(0);
@@ -94,47 +60,22 @@ export const Timer = ({ deadline }) => {
   return (
     <div>
       <p className="tasks__item__dateText">Remaining time:</p>
-      <div>
-        {deadline !== "Not set" ? (
-          <div>
-            {days === 0 ? (
-              <>
-                {minutes === 0 ? (
-                  <>
-                    {seconds === 0 ? (
-                      <></>
-                    ) : (
-                      <p className="tasks__item__clock">{` 00${animation}${seconds
-                        .toString()
-                        .padStart(2, "0")}`}</p>
-                    )}
-                  </>
-                ) : (
-                  <p className="tasks__item__clock">{` ${hours}${animation}${minutes
-                    .toString()
-                    .padStart(2, "0")}${animation}${seconds
-                    .toString()
-                    .padStart(2, "0")}`}</p>
-                )}
-              </>
-            ) : (
-              <p className="tasks__item__clock">{`${days}d ${hours}${animation}${minutes
-                .toString()
-                .padStart(2, "0")}${animation}${seconds
-                .toString()
-                .padStart(2, "0")}`}</p>
-            )}
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
       {deadline !== "Not set" ? (
         <>
-          {remainingTime <= 0 ? (
-            <p className="tasks__item__timeUp">TIME IS UP!!!</p>
-          ) : (
+          {days >= 1 ? (
             <p className="tasks__item__date">{remainingTime}</p>
+          ) : (
+            <>
+              {remainingTime <= 0 ? (
+                <p className="tasks__item__timeUp">TIME IS UP!!!</p>
+              ) : (
+                <p className="tasks__item__clock">{` ${hours}${animation}${minutes
+                  .toString()
+                  .padStart(2, "0")}${animation}${seconds
+                  .toString()
+                  .padStart(2, "0")}`}</p>
+              )}
+            </>
           )}
         </>
       ) : (
@@ -143,36 +84,3 @@ export const Timer = ({ deadline }) => {
     </div>
   );
 };
-
-// return (
-//     <div>
-//       {deadline !== "Not set" ? (
-//           <div>
-//             <p className="tasks__item__dateText">Remaining time:</p>
-//             {days === 0 ? (
-//                 <>
-//                   {minutes === 0 ? (
-//                       <>
-//                         {seconds === 0 ? (
-//                             <p className="tasks__item__timeUp">TIME IS UP!!!</p>
-//                         ) : (
-//                             <p className="tasks__item__date">{` ${minutes}m ${seconds}s`}</p>
-//                         )}
-//                       </>
-//                   ) : (
-//                       <p className="tasks__item__date">{` ${hours}h ${minutes}m ${seconds}s`}</p>
-//                   )}
-//                 </>
-//             ) : (
-//                 <p className="tasks__item__date">{`${days}d ${hours}h ${minutes}m ${seconds}s`}</p>
-//             )}
-//           </div>
-//       ) : (
-//           <>
-//             <p className="tasks__item__dateText">Remaining time:</p>
-//             <p className="tasks__item__date">No deadline set</p>
-//           </>
-//       )}
-//     </div>
-// );
-// };
